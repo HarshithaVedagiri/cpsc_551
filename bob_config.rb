c8913f43e45227b7f68f37f0dd9a99539d1f69aa
Harshitha Vedagiri <56275047+HarshithaVedagiri@users.noreply.github.com>
2019-11-25 18:08:17 -0800

    Update and rename config_bob.rb to bob_config.rb

--- bob_config.rb
+++ bob_config.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'optparse'
 
 def read_config
-  config_filename = 'bobTS.yaml'
+  config_filename = 'bob_TS.yaml'
 
   ARGV.options do |opts|
     opts.on('-c', '--config=file', String) { |val| config_filename = val }
