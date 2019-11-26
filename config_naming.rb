cd03dd3ed2eee449d66535e50550765a83c817ef
Harshitha Vedagiri <56275047+HarshithaVedagiri@users.noreply.github.com>
2019-11-25 18:22:24 -0800

    Update config_naming.rb

--- config_naming.rb
+++ config_naming.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'optparse'
 
 def read_config
-  config_filename = 'naming_serverTS.yaml'
+  config_filename = 'naming_server.yaml'
 
   ARGV.options do |opts|
     opts.on('-c', '--config=file', String) { |val| config_filename = val }
